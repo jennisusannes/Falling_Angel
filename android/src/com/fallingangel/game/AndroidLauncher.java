d298b2048bc1826bd70b40ff81de66a8983e2282
Jenni Susanne Skaara <jenniss@stud.ntnu.no>
2021-03-15 13:22:51 +0100

    Set up firebase, Ashley and LibGDX to new Android project

---
+++ android/src/com/fallingangel/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.fallingangel.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.fallingangel.game.FallingAngel;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FallingAngel(), config);
+	}
+}
