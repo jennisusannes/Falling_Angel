02b7c326185c3c7605cf978eb72659a022d7fec8
Ingrid Borkenhagen <ingrirbo@stud.ntnu.no>
2021-03-18 13:29:30 +0100

    tester dette

--- android/src/com/fallingangel/game/AndroidLauncher.java
+++ android/src/com/fallingangel/game/AndroidLauncher.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.fallingangel.game.FallingAngel;
+import com.fallingangel.FallingAngel;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
