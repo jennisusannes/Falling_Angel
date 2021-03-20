dd5d140b8a64723da95b65fa2e2def53c765c543
Josefine Stiff Aamlid <josefinestiffaamlid@dhcp-10-22-36-163.wlan.ntnu.no>
2021-03-20 14:59:06 +0100

    Restructuring ECS

--- core/src/com/fallingangel/model/ashleyECS/component/Animation.java
+++
@@ -1,7 +0,0 @@
-package com.fallingangel.model.ashleyECS.component;
-
-import com.badlogic.ashley.core.Component;
-
-public class Animation implements Component {
-
-}
--- core/src/com/fallingangel/model/component/TextureComponent.java
+++ core/src/com/fallingangel/model/component/TextureComponent.java
@@ -1,4 +1,4 @@
-package com.fallingangel.model.ashleyECS.component;
+package com.fallingangel.model.component;
 
 import com.badlogic.ashley.core.Component;
 
