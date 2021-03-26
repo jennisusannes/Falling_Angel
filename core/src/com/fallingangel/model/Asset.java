dfe7093c0c861f676bab9d0ae79af4e5ac9c2feb
Ingebwi <72627273+Ingebwi@users.noreply.github.com>
2021-03-26 08:40:34 +0100

    comments on assets and world

--- core/src/com/fallingangel/model/Asset.java
+++ core/src/com/fallingangel/model/Asset.java
@@ -1,4 +1,14 @@
 package com.fallingangel.model;
 
+import com.badlogic.gdx.graphics.Texture;
+
 public class Asset {
+
+
+    //TODO: implement methods for fetching assets
+    public static Texture background;
+
+
+
+
 }
--- core/src/com/fallingangel/model/World.java
+++ core/src/com/fallingangel/model/World.java
@@ -1,4 +1,81 @@
 package com.fallingangel.model;
 
+import com.badlogic.ashley.core.Component;
+import com.badlogic.ashley.core.Engine;
+import com.badlogic.ashley.core.Entity;
+import com.fallingangel.model.component.AngelComponent;
+import com.fallingangel.model.component.CoinComponent;
+import com.fallingangel.model.component.ObstacleComponent;
+import com.fallingangel.model.component.PlaneComponent;
+import com.fallingangel.model.component.PowerUpComponent;
+
 public class World {
+
+
+    //Må finne riktige statiske variabler her, sånn at appen går helt ut i kanten på alle devices
+    public static final float WORLD_HEIGHT = 15*20;
+    public static final float WORLD_WIDTH = 10;
+
+    /*
+    //Usikker på om vi skal ha med disse variablene, men lar de stå her:
+    public static final int WORLD_STATE_RUNNING = 0;
+	public static final int WORLD_STATE_NEXT_LEVEL = 1;
+	public static final int WORLD_STATE_GAME_OVER = 2;
+	public static final Vector2 gravity = new Vector2(0, -12);
+     */
+
+    private Engine engine;
+
+    //Er en mulighet for at det er lurere å bruke pooled engine.
+    public World(Engine engine){
+        this.engine = engine;
+    }
+
+
+    public Entity createAngel(){
+        Entity angelEntity = new Entity();
+        AngelComponent ac = new AngelComponent();
+        angelEntity.add(ac);
+        engine.addEntity(angelEntity);
+        return angelEntity;
+    }
+
+    public Entity createCoin(){
+        Entity coinEntity = new Entity();
+        CoinComponent cc = new CoinComponent();
+        coinEntity.add(cc);
+        engine.addEntity(coinEntity);
+        return coinEntity;
+    }
+
+    public Entity createObstacle(){
+        Entity obstacleEntity = new Entity();
+        ObstacleComponent oc = new ObstacleComponent();
+        obstacleEntity.add(oc);
+        engine.addEntity(obstacleEntity);
+        return obstacleEntity;
+    }
+
+
+    public Entity createPlane(){
+        Entity planeEntity = new Entity();
+        PlaneComponent pc = new PlaneComponent();
+        planeEntity.add(pc);
+        engine.addEntity(planeEntity);
+        return planeEntity;
+    }
+
+    public Entity powerUp(){
+        Entity powerup = new Entity();
+        PowerUpComponent puc = new PowerUpComponent();
+        powerup.add(puc);
+        engine.addEntity(powerup);
+        return powerup;
+    }
+
+    //TODO: er det flere som skal implementeres?
+
+
+
+
 }
