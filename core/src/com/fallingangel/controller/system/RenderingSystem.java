60eb8c2ecdc5210e4a73d7ac98c5d8603f930cdf
Ingebwi <72627273+Ingebwi@users.noreply.github.com>
2021-04-08 16:27:35 +0200

    comments and clean up

--- core/src/com/fallingangel/controller/system/RenderingSystem.java
+++ core/src/com/fallingangel/controller/system/RenderingSystem.java
@@ -7,7 +7,6 @@ import com.badlogic.ashley.systems.IteratingSystem;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.utils.Array;
 import com.fallingangel.model.component.TextureComponent;
 import com.fallingangel.model.component.TransformComponent;
@@ -16,9 +15,9 @@ import java.util.Comparator;
 
 public class RenderingSystem extends IteratingSystem{
 
+    //RenderingSystem
 
-    //Skjønner ikke helt hva "FRUSTUM" betyr ...
-    //skjønner ikke helt bruken (?)
+
     static final float FRUSTUM_WIDTH = 10;
     static final float FRUSTUM_HEIGHT = 15;
     static final float PIXELS_TO_METRES = 1.0f / 32.0f;
@@ -28,79 +27,81 @@ public class RenderingSystem extends IteratingSystem{
     private Comparator<Entity> comparator;
     private OrthographicCamera cam;
 
-    private ComponentMapper<TextureComponent> textureM;
-    private ComponentMapper<TransformComponent> transformM;
+    private ComponentMapper<TextureComponent> textureMapper;
+    private ComponentMapper<TransformComponent> transformMapper;
 
 
-    //Ser at denne konstruktøren tar inn family som argument, mens konstruktøren i SuperJumper tar inn sb
-    /*
-    public RenderingSystem(Family family) {
-        super(family);
-    }
-
-     */
-
-
+    //Instanciate a RenderingSystem
     public RenderingSystem(SpriteBatch sb) {
+        //Creates a new iteratingSystem for the entities with following components
         super(Family.all(TransformComponent.class, TextureComponent.class).get());
 
-        textureM= ComponentMapper.getFor(TextureComponent.class);
-        transformM = ComponentMapper.getFor(TransformComponent.class);
+        //Gets the components through a mapper
+        textureMapper = ComponentMapper.getFor(TextureComponent.class);
+        transformMapper = ComponentMapper.getFor(TransformComponent.class);
 
+        //Creates a renderqueue
         renderQueue = new Array<Entity>();
 
-        //sorts based on z-value. the highest z-value will be in the back of the screen.
+        //sorts based on z-value: the highest z-value will be in the back of the screen (Det bakerste laget)
         comparator = new Comparator<Entity>() {
             @Override
             public int compare(Entity entityA, Entity entityB) {
-                return (int)Math.signum(transformM.get(entityB).pos.z -
-                        transformM.get(entityA).pos.z); //returns -1, 0 or 1 based on which z-value is the largest
+                return (int)Math.signum(transformMapper.get(entityB).pos.z -
+                        transformMapper.get(entityA).pos.z); //returns -1, 0 or 1 based on which z-value is the largest
             }
         };
 
         this.sb = sb;
 
+        // ?
         cam = new OrthographicCamera(FRUSTUM_WIDTH, FRUSTUM_HEIGHT);
         cam.position.set(FRUSTUM_WIDTH / 2, FRUSTUM_HEIGHT / 2, 0);
     }
 
+    //This method updates and draws
     @Override
     public void update(float deltaTime) {
+        //Updates the iteratingSystem
         super.update(deltaTime);
 
+        //Sort the renderQueue based on comparator (z-axis)
         renderQueue.sort(comparator); //the entities are being sorted based on z-value
 
+
         cam.update();
-        //sb.setProjectionMatrix(cam.combined); //denne zoomer inn kameraet, vet ikke om den skal brukes enda
+        //denne zoomer inn kameraet, vet ikke om den skal brukes enda
+        //sb.setProjectionMatrix(cam.combined);
         sb.begin();
 
-        //0 bakgrunn, 1 plane, 2 obstacle (balloon), 3 angel,
-        //sb.draw(renderQueue.get(4).getComponent(TextureComponent.class).textureRegion, 0, 0, 800, 800);
-
+        //iterates through all entities and gets the component.
         for (Entity entity : renderQueue) {
-            TextureComponent tex = textureM.get(entity);
+            TextureComponent textureComponent = textureMapper.get(entity);
+            TransformComponent transformComponent = transformMapper.get(entity);
 
-            if (tex.textureRegion == null) {
+            //Check if contains textureRegion
+            if (textureComponent.textureRegion == null) {
                 continue;
             }
 
-            TransformComponent t = transformM.get(entity);
 
-
-            float width = tex.textureRegion.getRegionWidth();
-            float height = tex.textureRegion.getRegionHeight();
+            //Not used yet
+            float width = textureComponent.textureRegion.getRegionWidth();
+            float height = textureComponent.textureRegion.getRegionHeight();
             float originX = width * 0.5f;
             float originY = height * 0.5f;
 
+            //TODO: create methods for drawing
             /*
-            sb.draw(tex.textureRegion,
-                    t.pos.x - originX, t.pos.y - originY,
+            sb.draw(textureComponent.textureRegion,
+                    transformComponent.pos.x - originX, transformComponent.pos.y - originY,
                     originX, originY,
                     width, height,
-                    t.scale.x * PIXELS_TO_METRES, t.scale.y * PIXELS_TO_METRES,
-                    MathUtils.radiansToDegrees * t.rotation);*/
+                    transformComponent.scale.x * PIXELS_TO_METRES, transformComponent.scale.y * PIXELS_TO_METRES,
+                    MathUtils.radiansToDegrees * transformComponent.rotation);*/
 
-            sb.draw(tex.textureRegion, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+            //Working code, remove when method is up and running:
+            sb.draw(textureComponent.textureRegion, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 
         }
 
@@ -109,6 +110,7 @@ public class RenderingSystem extends IteratingSystem{
     }
 
 
+    //called at every render/ update. Adds all entities from family in the queue
     @Override
     public void processEntity(Entity entity, float deltaTime) {
         renderQueue.add(entity);
