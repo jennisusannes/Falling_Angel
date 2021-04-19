411494d9d6d7892d78af3a59480aa59d044fe976
Jenni Susanne Skaara <jenniss@stud.ntnu.no>
2021-04-19 16:31:52 +0200

    Waiting room is in order!!!

--- core/src/com/fallingangel/view/MultiPlayerView.java
+++ core/src/com/fallingangel/view/MultiPlayerView.java
@@ -103,6 +103,8 @@ public class MultiPlayerView extends ScreenAdapter {
             //send to database
             FallingAngel.getInstance().FBI.connectToRoom(roomListener.room, multiPlayerData);
             alreadyConnected = true;
+
+            FallingAngel.getInstance().FBI.numberOfUsersInRoom();
         }
     }
 
